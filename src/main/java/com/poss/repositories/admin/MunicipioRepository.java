be95a2bf17a85b373baf6fa11037a54562b8b45c
JuliusMatadamas <julius3877@gmail.com>
2025-08-21 17:29:58 -0500

    Se corrige bug en el metodo findAll de Municipio

--- src/main/java/com/poss/repositories/admin/MunicipioRepository.java
+++ src/main/java/com/poss/repositories/admin/MunicipioRepository.java
@@ -2,6 +2,9 @@ package com.poss.repositories.admin;
 
 import com.poss.entities.admin.Municipio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +12,14 @@ import java.util.List;
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Integer> {
 
-    List<Municipio> findByNombreContainingIgnoreCase(String nombre);
-    List<Municipio> findByEstadoId(Integer id);
+    @Query("SELECT m FROM Municipio m JOIN FETCH m.estado")
+    @Override
+    @NonNull
+    List<Municipio> findAll();
+
+    @Query("SELECT m FROM Municipio m JOIN FETCH m.estado WHERE lower(m.nombre) LIKE lower(concat('%', :nombre, '%'))")
+    List<Municipio> findByNombreContainingIgnoreCase(@Param("nombre") String nombre);
+
+    @Query("SELECT m FROM Municipio m JOIN FETCH m.estado e WHERE e.id = :id")
+    List<Municipio> findByEstadoId(@Param("id") Integer id);
 }
